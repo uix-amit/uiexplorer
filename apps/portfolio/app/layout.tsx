215077b84364eeb54e83148e4b2a4d3a9c2dc3dc
uix-amit <amit.uiexplorer@gmail.com>
2024-02-03 18:38:33 +0530

    seo uplifting

--- apps/portfolio/app/layout.tsx
+++ apps/portfolio/app/layout.tsx
@@ -1,11 +1,5 @@
 import './global.css';
 
-export const metadata = {
-  title: 'Amit Kumbharkar',
-  description:
-    'Amit Kumbharkar, UI Developer, reactjs, nextjs, react, SASS, Angular, NgRx, RxJs, API Integration, NextJs, MFE, Micro Front-end, JavaScript, HTML5, Cascading Style Sheets, tailwind, SCSS, CSS, Git, jQuery, Front-end Development, Node.js, Bootstrap, TypeScript, Web Development, HTML, Responsive Web Design',
-};
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
@@ -13,6 +7,36 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta
           name='google-site-verification'
           content='9HBSiJfal9nid2wjQwDZQe69gSmQkisi3vC3JBhmzLs'
+        />
+        <title>Amit Kumbharkar</title>
+        <meta
+          name='description'
+          content='Amit Kumbharkar, UI Developer, reactjs, nextjs, react, SASS, Angular, NgRx, RxJs, API Integration, NextJs, MFE, Micro Front-end, JavaScript, HTML5, Cascading Style Sheets, tailwind, SCSS, CSS, Git, jQuery, Front-end Development, Node.js, Bootstrap, TypeScript, Web Development, HTML, Responsive Web Design'
+        />
+
+        <meta property='og:url' content='https://uiexplorer.netlify.app/' />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content='Amit Kumbharkar' />
+        <meta
+          property='og:description'
+          content='Amit Kumbharkar, UI Developer, reactjs, nextjs, react, SASS, Angular, NgRx, RxJs, API Integration, NextJs, MFE, Micro Front-end, JavaScript, HTML5, Cascading Style Sheets, tailwind, SCSS, CSS, Git, jQuery, Front-end Development, Node.js, Bootstrap, TypeScript, Web Development, HTML, Responsive Web Design'
+        />
+        <meta
+          property='og:image'
+          content='https://opengraph.b-cdn.net/production/documents/5f2b946a-6675-4443-8a7a-87c68fa0701b.jpg?token=U-ef5-P8BW0Hb-1D__9Wtl7KqG4A1r_inJVuO0w-zXw&height=350&width=770&expires=33242965394'
+        />
+
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta property='twitter:domain' content='uiexplorer.netlify.app' />
+        <meta property='twitter:url' content='https://uiexplorer.netlify.app/' />
+        <meta name='twitter:title' content='Amit Kumbharkar' />
+        <meta
+          name='twitter:description'
+          content='Amit Kumbharkar, UI Developer, reactjs, nextjs, react, SASS, Angular, NgRx, RxJs, API Integration, NextJs, MFE, Micro Front-end, JavaScript, HTML5, Cascading Style Sheets, tailwind, SCSS, CSS, Git, jQuery, Front-end Development, Node.js, Bootstrap, TypeScript, Web Development, HTML, Responsive Web Design'
+        />
+        <meta
+          name='twitter:image'
+          content='https://opengraph.b-cdn.net/production/documents/5f2b946a-6675-4443-8a7a-87c68fa0701b.jpg?token=U-ef5-P8BW0Hb-1D__9Wtl7KqG4A1r_inJVuO0w-zXw&height=350&width=770&expires=33242965394'
         />
       </head>
       <body className='ue-flex ue-flex-col ue-items-center ue-py-24 ue-bg-slate-900 ue-text-slate-400 ue-p-4'>
